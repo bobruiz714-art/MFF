a4620e79ad7f2b070681fa029cc80556f6c9fe31
bobruiz714-art <bobruiz714@gmail.com>
2025-09-07 23:03:14 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,9 +18,9 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 linked_accounts_file = "linked_accounts.json"
 CONFIG_FILE = "config.json"
-ADMIN_ROLE_NAME = "🔨Mod"
+ADMIN_ROLE_NAME = "MFF"
 SUPPORTER_ROLE_NAME = "Supporter"
-OWNER_ID = 1322627642746339432
+OWNER_ID = 1056801828467114076
 BACKEND_URL = "https://backend-2-0-9uod.onrender.com/redeem"  # Replace with your actual endpoint
 ROBLOX_API_URL = "https://inventory.roblox.com/v1/users/{user_id}/items/GamePass/{gamepass_id}"
 
@@ -423,3 +423,4 @@ async def main():
 if __name__ == "__main__":
     load_dotenv()
     asyncio.run(main())
+
